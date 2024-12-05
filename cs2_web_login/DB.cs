8adc4d0c0822de262fef06fb894ccca4792a97f0
OwnSample <ownsample@tutanota.com>
2024-12-05 23:21:18 +0100

    change db defaults

--- cs2_web_login/DB.cs
+++ cs2_web_login/DB.cs
@@ -2,21 +2,16 @@ namespace cs2_web_login;
 
 public class DB
 {
-	public string Host { get; set; } = "localhost";
+  public string Host { get; set; } = "localhost";
 
+  public string User { get; set; } = "db_user";
 
-	public string User { get; set; } = "db_user";
+  public string Password { get; set; } = "password";
 
+  public string Database { get; set; } = "database";
 
-	public string Password { get; set; } = "password";
+  public string Table { get; set; } = "user";
 
-
-	public string Database { get; set; } = "database";
-
-
-	public string Table { get; set; } = "life_less_idiots";
-
-
-	public string Prefix { get; set; } = "os_";
+  public string Prefix { get; set; } = "";
 
 }
